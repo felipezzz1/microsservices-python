2630ac81386f19ff4ed6ccd1ac98cf8bdc012e50
felipezzz1 <felipezorzo78@gmail.com>
2024-10-14 09:44:31 -0300

    adding build

--- main.py
+++ main.py
@@ -7,30 +7,35 @@ from mylib.logic import phrase as phrase_logic
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Wikipedia API. Call /search or /wiki or /phrase"}
+
 
 @app.get("/search/{value}")
 async def search(value: str):
     """Page to search in wikipedia"""
 
     result = search_wiki(value)
-    return {"search" : result}
+    return {"search": result}
+
 
 @app.get("/wiki/{value}")
 async def wiki(value: str):
     """Retrieve wikipedia page"""
 
     result = wiki_logic(value)
-    return {"wiki" : result}
+    return {"wiki": result}
+
 
 @app.get("/phrase/{value}")
 async def phrase(value: str):
     """Retrieve wikipedia page and return phrases"""
 
     result = phrase_logic(value)
-    return {"phrase" : result}
+    return {"phrase": result}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
--- test_main.py
+++ test_main.py
@@ -1,26 +1,55 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
+
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Wikipedia API. Call /search or /wiki or /phrase"}
+    assert response.json() == {
+        "message": "Wikipedia API. Call /search or /wiki or /phrase"
+    }
+
 
 def test_read_search():
     # this actually will fail
     response = client.get("/search/Brazil")
     assert response.status_code == 200
-    assert response.json() == {"search":["Brazil","Brazil national football team","Brazilian","Brazilians","Brazil (disambiguation)","Federative units of Brazil","Municipalities of Brazil","Brazil nut","Brazilian real","Regions of Brazil"]}
+    assert response.json() == {
+        "search": [
+            "Brazil",
+            "Brazil national football team",
+            "Brazilian",
+            "Brazilians",
+            "Brazil (disambiguation)",
+            "Federative units of Brazil",
+            "Municipalities of Brazil",
+            "Brazil nut",
+            "Brazilian real",
+            "Regions of Brazil",
+        ]
+    }
+
 
 def test_read_wiki():
     response = client.get("/wiki/Brazil")
     assert response.status_code == 200
-    assert response.json() == {"wiki": "Brazil, officially the Federative Republic of Brazil, is the largest and easternmost country in South America and Latin America."}
+    assert response.json() == {
+        "wiki": "Brazil, officially the Federative Republic of Brazil, is the largest and easternmost country in South America and Latin America."
+    }
+
 
 def test_read_phrase():
     response = client.get("/phrase/Brazil")
     assert response.status_code == 200
-    assert response.json() == {"phrase":["brazil","federative","brazil","easternmost country","america","america"]}
+    assert response.json() == {
+        "phrase": [
+            "brazil",
+            "federative",
+            "brazil",
+            "easternmost country",
+            "america",
+            "america",
+        ]
+    }
